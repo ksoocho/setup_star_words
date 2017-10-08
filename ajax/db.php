2ef24e35eb5ac49668725c435d676c5f93b324fc
Kyoung Soo, cho <ksoocho@naver.com>
2017-10-08 11:10:57 +0900

    Update db.php

--- ajax/db.php
+++ ajax/db.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'cksoonew');
-define('DB_PASSWORD', 'cks13837!');
+define('DB_USERNAME', 'xxxxxx');
+define('DB_PASSWORD', 'xxxxxx!');
 define('DB_DATABASE', 'cksoonew');
 
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
